006cfa68e1f5938c8970155685a052eae2e187ad
Shubh280411 <shubh280411@gmail.com>
2026-07-14 16:06:54 +0530

    Add: public footer to all user and admin pages

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { UserSidebar } from '@/components/layout/user-sidebar';
 import { UserHeader } from '@/components/layout/user-header';
+import { PublicFooter } from '@/components/layout/public-footer';
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,9 @@ export default function UserLayout({ children }: { children: React.ReactNode }) 
         <UserHeader />
         <main className="flex-1 overflow-y-auto p-4 lg:p-6">
           {children}
+          <div className="mt-8 pt-6 border-t border-[rgba(0,229,255,0.06)]">
+            <PublicFooter />
+          </div>
         </main>
       </div>
     </div>
