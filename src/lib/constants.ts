1e47ae8e3662f82d220d6d78c431439b0d003053
Shubh280411 <shubh280411@gmail.com>
2026-08-28 00:51:03 +0530

    chore: update treasury wallet address for presale deposits

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -109,7 +109,7 @@ export const REBUY_MAX = 2;
 export const APP_VERSION = '1.0.0';
 
 export const USDT_ADDRESS = '0x55d398326f99059fF775485246999027B3197955';
-export const TREASURY_WALLET = '0xb0bf2a92b33caa2de9c4a24836eadb137fe77373';
+export const TREASURY_WALLET = '0x1496A6bdD616DA866661cEF70E84B1115C654c72';
 export const BSC_RPC_URL = 'https://bsc-dataseed.binance.org/';
 export const BSC_CHAIN_ID = 56;
 export const USDT_DECIMALS = 18;
